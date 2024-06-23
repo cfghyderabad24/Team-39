93aacf30b06ecf08235783f710ce00ab3a4fa238
RSnehitha <snehithareddybathula@gmail.com>
2024-06-23 10:46:29 +0530

    updated admindashboard

--- client/src/components/AdminDashboard/AdminDashboard.js
+++ client/src/components/AdminDashboard/AdminDashboard.js
@@ -24,8 +24,13 @@ const AdminDashboard = () => {
 
   return (
     <div className="container d-flex flex-column align-items-center vh-100">
-      <h1 className="text-center mx-auto justify-content-center border-2 p-2 border-cyan-300 mt-4 bg-cyan-100 rounded-lg">Jaldhaara Foundation</h1>
-      <button className="btn btn-outline-primary text-lg mt-3" onClick={handleButtonClick}>DONORS</button>
+    {/* Buttons */}
+      <h1 className="text-center mx-auto justify-content-center border-2 p-2 border-cyan-300 mt-4 bg-cyan-100 rounded-lg">
+      Jaldhaara Foundation
+      </h1>
+      <button className="btn btn-outline-primary text-lg mt-3" onClick={handleButtonClick}>
+      DONORS
+      </button>
       <div className="mt-auto mb-4">
         <button className="btn btn-outline-primary text-lg" onClick={handleButtonClick}>Download</button>
       </div>
